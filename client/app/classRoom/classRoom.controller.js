40ea994de8453f9fad75f5cceaeb5801732dff10
Raju Chikkireddy <Raju Chikkireddy>
2016-11-13 17:08:12 +0530

    added class room updation code

--- client/app/classRoom/classRoom.controller.js
+++ client/app/classRoom/classRoom.controller.js
@@ -1,11 +1,15 @@
 'use strict';
 
 class classRoomCtrl{
-	constructor($uibModalInstance) {
+	constructor($uibModalInstance,cr) {
 		this.$uibModalInstance = $uibModalInstance;
+		this.cr=cr;
 	}
 	cancel(){
 		this.$uibModalInstance.dismiss('dismiss reason');
+	}
+	saveThisClassRoom(){
+		this.$uibModalInstance.close(this.cr);
 	}
 }
 angular.module('authCellApp')

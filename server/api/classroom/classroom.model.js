3ac8da9e84e56c613a62ccb27c3507c3e5bfeb15
Ganesh <ganeshbonangi@users.noreply.github.com>
2017-07-21 17:55:45 +0530

    Update classroom.model.js

--- server/api/classroom/classroom.model.js
+++ server/api/classroom/classroom.model.js
@@ -5,6 +5,7 @@ import mongoose from 'mongoose';
 var ClassroomSchema = new mongoose.Schema({
   name: String,
   admin: String,
+  disconnect:{type:Boolean,default:false},
   students: [{
   	name:String,
   	id:String

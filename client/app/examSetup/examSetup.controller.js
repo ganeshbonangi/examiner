9e3eb120a4beb87d2a49364a89ba768e7e59acd2
Ganesh Bonangi <ganeshbonangi@ganeshs-MacBook-Air.local>
2017-07-25 12:09:37 +0530

    added exam setup

--- client/app/examSetup/examSetup.controller.js
+++ client/app/examSetup/examSetup.controller.js
@@ -11,13 +11,6 @@ class examSetupCtrl {
             opened: false
         };
         this.format = 'dd-MMMM-yyyy';
-        this.dateOptions = {
-            dateDisabled: false,
-            formatYear: 'yy',
-            maxDate: new Date(2020, 5, 22),
-            minDate: new Date(),
-            startingDay: 1
-        };
         this.open1 = function() {
             this.popup1.opened = true;
         };
@@ -30,12 +23,32 @@ class examSetupCtrl {
             exam.expiredate = utilService.getDateObject(exam.expiredate);
             this.exam = exam;
             this.upDateTheExamVar = true;
+            if(!this.exam.classRooms){
+                this.exam.classRooms = [];
+            }
         } else {
             this.exam = {
                 hstep: 1,
-                mstep: 5
+                mstep: 5,
+                classRooms:[],
+                expiredate:null,
+                activationdate:null
             };
         }
+        this.dateOptions = {
+            dateDisabled: false,
+            formatYear: 'yy',
+            maxDate: this.exam.expiredate,
+            minDate: new Date(),
+            startingDay: 1
+        };
+        this.endDateOptions = {
+            dateDisabled: false,
+            formatYear: 'yy',
+            maxDate: new Date(2020, 5, 22),
+            minDate: this.exam.activationdate,
+            startingDay: 1
+        };
     }
     cancel() {
         this.$uibModalInstance.dismiss('dismiss reason');
@@ -48,6 +61,14 @@ class examSetupCtrl {
             this.suggestedClassRooms = response.data;
             this.showList = true;
         });
+    }
+    removeClassRoomFromExam(index){
+        this.exam.classRooms.splice(index,1);
+    }
+    addClassRoomToExam(clsObj){
+        this.exam.classRooms.push(clsObj);
+        this.showList = false;
+        this.className = '';
     }
   updateTheExam() {
   //has to make service call for update exam

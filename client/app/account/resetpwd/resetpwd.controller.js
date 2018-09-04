d0da25aa506cee350cf756b9c088a799085e32ab
Rajesh Ponmudi <rponmudi@prokarma.com>
2018-09-04 16:48:02 -0500

    introduced messages for the reset password

--- client/app/account/resetpwd/resetpwd.controller.js
+++ client/app/account/resetpwd/resetpwd.controller.js
@@ -8,10 +8,16 @@ class ResetpwdController {
 
   resetPassword(form) {
     this.submitted = true;
+    this.isSuccessfullyResetPwd = false;
     if (form.$valid) {
       this.$http.post('api/users/forgotPassword',{'confirmPassword':this.user.confirmPassword, 'newPassword':this.user.newPassword, 'resetPasswordToken':this.$stateParams.token})
       .then((response)=>{
+        this.isSuccessfullyResetPwd = true;
+        this.message = 'Successfully password reset.'
         console.log(response);
+      }).catch(()=>{
+        this.isSuccessfullyResetPwd = false;
+        this.message = 'Failed to reset password.'
       });
     }
   }

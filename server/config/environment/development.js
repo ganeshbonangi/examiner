547d7275ec39526fa899068eb45d50fe33e4782f
ganeshbonangi <ganesh.bonangi@gmail.com>
2019-06-21 01:48:43 +0530

    bug fixes

--- server/config/environment/development.js
+++ server/config/environment/development.js
@@ -6,7 +6,7 @@ module.exports = {
 
   // MongoDB connection options
   mongo: {
-    uri: 'mongodb://localhost/examcolors',
+    uri: 'mongodb://ganesh007:ganesh007@ds061208.mlab.com:61208/examcolors',//'mongodb://localhost/examcolors',
     options:{
       useMongoClient: true
     },
